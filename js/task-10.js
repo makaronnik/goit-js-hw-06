bef25cd4fe98e61be2fae73bd7094f13f9d9373c
Andriy Karpishin <mkrnnk@gmail.com>
2023-02-19 15:25:48 +0200

    Task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,52 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  createButtonEl: document.querySelector('[data-create]'),
+  destroyButtonEl: document.querySelector('[data-destroy]'),
+  numberInputEl: document.querySelector('#controls input'),
+  boxesDivEl: document.querySelector('#boxes'),
+};
+let divSideLength = 30;
+
+refs.createButtonEl.addEventListener('click', onCreateButtonClick);
+refs.destroyButtonEl.addEventListener('click', onDestroyButtonClick);
+
+function onCreateButtonClick() {
+  const numberOfDivElementsToCreate = refs.numberInputEl.value;
+
+  createBoxes(numberOfDivElementsToCreate);
+}
+
+function onDestroyButtonClick() {
+  destroyBoxes();
+}
+
+function createBoxes(number) {
+  const divElements = createDivElements(number);
+
+  refs.boxesDivEl.append(...divElements);
+}
+
+function destroyBoxes() {
+  refs.boxesDivEl.innerHTML = '';
+}
+
+function createDivElements(number) {
+  const divElements = [];
+
+  for (let index = 0; index < number; index++) {
+    const divElement = document.createElement('div');
+
+    divElement.style.height = `${divSideLength}px`;
+    divElement.style.width = `${divSideLength}px`;
+    divElement.style.backgroundColor = getRandomHexColor();
+
+    divElements.push(divElement);
+
+    divSideLength += 10;
+  }
+
+  return divElements;
+}
