6096feb3b0d64a8f8aa886eabdb73bc83da0d413
Andriy Karpishin <mkrnnk@gmail.com>
2023-02-19 12:34:51 +0200

    Task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const listElement = document.querySelector('#ingredients');
+
+const createdElements = ingredients.map(title => {
+  const listItem = document.createElement('li');
+
+  listItem.textContent = title;
+  listItem.classList.add('item');
+
+  return listItem;
+});
+
+listElement.append(...createdElements);
