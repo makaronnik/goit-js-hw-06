1e0dd69da671482f2949986132a53bbe39424192
Andriy Karpishin <mkrnnk@gmail.com>
2023-02-19 14:49:02 +0200

    Task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  colorSpanEl: document.querySelector('.color'),
+  changeColorButtonEl: document.querySelector('.change-color'),
+};
+
+refs.changeColorButtonEl.addEventListener('click', onChangeColorButtonClick);
+
+function onChangeColorButtonClick() {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  refs.colorSpanEl.textContent = randomColor;
+}
