6444dace223ff6b1e467d331aae65d8b081b0296
Andriy Karpishin <mkrnnk@gmail.com>
2023-02-19 14:38:35 +0200

    Task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!checkTheFieldsAreFilled(this.elements)) {
+    alertFieldsAreNotFilled();
+
+    return;
+  }
+
+  console.log(composeFormValuesObject(this.elements));
+
+  this.reset();
+}
+
+function checkTheFieldsAreFilled(elements) {
+  return elements.email.value.length > 0 && elements.password.value.length > 0;
+}
+
+function alertFieldsAreNotFilled() {
+  alert('Не всі поля заповнені! Заповніть будь ласка.');
+}
+
+function composeFormValuesObject(elements) {
+  return {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+}
